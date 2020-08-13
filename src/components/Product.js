447bd6cfd02fe4cedb840fded2d3a3fe07e098dd
Nestor <ncervan87@gmail.com>
2020-08-13 15:36:16 -0700

    add feature that prevents user from adding same item to cart twice

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { CartContext } from '../contexts/CartContext'
 
 const Product = props => {
+	const { cart } = useContext(CartContext)
+
+	let inCart = false
+
+	cart.forEach(item => {
+		if (item.id === props.product.id) {
+			inCart = true
+		}
+	})
+
 	return (
 		<div className="product">
 			<img src={props.product.image} alt={`${props.product.title} book`} />
@@ -9,8 +20,8 @@ const Product = props => {
 
 			<p className="price">${props.product.price}</p>
 
-			<button onClick={() => props.addItem(props.product)}>
-				Add to cart
+			<button style={{ background: inCart ? 'lightcoral' : null, color: inCart ? 'white' : null }} disabled={inCart} onClick={() => props.addItem(props.product)}>
+				{inCart ? 'In cart' : 'Add to cart'}
 			</button>
 		</div>
 	);
